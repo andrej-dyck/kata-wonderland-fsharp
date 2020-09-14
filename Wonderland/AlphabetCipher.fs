0de7392ecbf71e8813b549c5e115e9f3dea08a2c
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2020-09-14 10:25:33 +0200

    Implement alphabet cipher dencoding

--- Wonderland/AlphabetCipher.fs
+++ Wonderland/AlphabetCipher.fs
@@ -9,27 +9,32 @@ module AlphabetCipher =
     let private alphabet = [| 'a' .. 'z' |]
     let private alphabetLength = Array.length alphabet
 
-    let private indexOf (alphabet: char []) (c: char) =
-        let intValueOfFirstChar = int (Array.head alphabet)
 
-        (int c) - intValueOfFirstChar
+    let private indexIn (alphabet: char []) (c: char) = (int c) - (int (Array.head alphabet))
 
-    let private indexInAlphabet = indexOf alphabet
+    let private indexInAlphabet = indexIn alphabet
 
-    let private substituteChar (keyChar: char) (messageChar: char) =
-        let indexMessageChar = indexInAlphabet messageChar
-        let indexKeyChar = indexInAlphabet keyChar
 
-        alphabet.[(indexKeyChar + indexMessageChar) % alphabetLength]
+    let private lookupInAlphabet (buildIndexOffset) (keyChar: char) (messageChar: char) =
+        alphabet.[buildIndexOffset (indexInAlphabet keyChar) (indexInAlphabet messageChar) % alphabetLength]
+
+    let private substituteChar =
+        lookupInAlphabet (fun indexKeyChar indexMessageChar -> indexKeyChar + indexMessageChar)
+
+    let private decodeChar =
+        lookupInAlphabet (fun indexKeyChar indexMessageChar -> indexMessageChar - indexKeyChar + alphabetLength)
+
 
     let private applyByIndex (f: char -> char -> char) (key: Keyword) (index: int) = f key.[index % key.Length]
 
-
-    let encode (key: Keyword) (message: Message): Message =
+    let private apply (f: char -> char -> char) (key: Keyword) (message: Message) =
         message
-        |> Seq.mapi (fun i c -> applyByIndex substituteChar key i c)
+        |> Seq.mapi (fun i c -> applyByIndex f key i c)
         |> System.String.Concat
 
-    let decode (key: Keyword) (message: Message): Message = "decodeme"
+
+    let encode (key: Keyword) (message: Message): Message = apply substituteChar key message
+
+    let decode (key: Keyword) (cipher: Message): Message = apply decodeChar key cipher
 
     let decipher (cipher: Message) (message: Message): Keyword = "decypherme"
