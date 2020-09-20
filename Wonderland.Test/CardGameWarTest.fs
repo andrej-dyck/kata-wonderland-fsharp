4c6f951f5c73997c7ea5b45cc06d112bd3773c0e
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2020-09-20 11:04:04 +0200

    Add card-game-war task

---
+++ Wonderland.Test/CardGameWarTest.fs
@@ -0,0 +1,34 @@
+﻿namespace Wonderland
+
+open Xunit
+open FsUnit.Xunit
+
+module CardGameWarTest =
+
+    open CardGameWar
+
+    let TODO = ()
+
+    [<Fact>]
+    let ``the highest rank wins the cards in the round`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``queens are higher rank than jacks`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``kings are higher rank than queens`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``aces are higher rank than kings`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``if the ranks are equal, clubs beat spades`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``if the ranks are equal, diamonds beat clubs`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``if the ranks are equal, hearts beat diamonds`` () = shouldFail (fun () -> TODO)
+
+    [<Fact>]
+    let ``the player loses when they run out of cards`` () = shouldFail (fun () -> TODO)
---
+++ Wonderland/CardGameWar.fs
@@ -0,0 +1,34 @@
+﻿namespace Wonderland
+
+module CardGameWar =
+
+    type Suit =
+        | Spade
+        | Club
+        | Diamond
+        | Heart
+
+    type Rank =
+        | Value of int
+        | Jack
+        | Queen
+        | King
+        | Ace
+
+    type Card = Suit * Rank
+
+    let playRound (card1: Card, card2: Card) = failwith "not implemented: winning card"
+
+    let playGame (hand1: Card list, hand2: Card list) = failwith "not implemented: game winner"
+
+(*
+    let suits = [ Spade; Club; Diamond; Heart ]
+    let heads = [ Jack; Queen; King; Ace ]
+    let ranks =
+        [   for v in 2 .. 10 -> Value v
+            for head in heads -> head
+        ]
+    let deck = seq {
+        for suit in suits do
+            for rank in ranks -> suit,rank }
+*)
