84cc1b6f5822904f31078aaa3ed412b9068525d4
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2020-09-28 08:43:55 +0200

    Implement winner of card game

--- Wonderland.Test/CardGameWarTest.fs
+++ Wonderland.Test/CardGameWarTest.fs
@@ -7,6 +7,8 @@ module CardGameWarTest =
 
     open CardGameWar
 
+    (* winning card *)
+
     type cardsWinningByRank() as this =
         inherit TheoryData<Card, Card, Card>() do
             this.Add((Club, Value 2), (Club, Value 3), (Club, Value 3))
@@ -69,9 +71,40 @@ module CardGameWarTest =
 
     [<Fact>]
     let ``there can be no to equal cards`` () =
-        shouldFail(fun () -> winningCard (Club, Value 2) (Club, Value 2) |> ignore)
+        shouldFail (fun () -> winningCard (Club, Value 2) (Club, Value 2) |> ignore)
 
-    let TODO = ()
+    (* winner of card game *)
 
     [<Fact>]
-    let ``the player loses when they run out of cards`` () = shouldFail (fun () -> TODO)
+    let ``player 1 wins when player 2 runs out of cards`` () =
+        winnerOfGame [ (Club, Value 2) ] []
+        |> should equal Player1
+
+    [<Fact>]
+    let ``player 2 wins when player 1 runs out of cards`` () =
+        winnerOfGame [] [ (Club, Value 2) ]
+        |> should equal Player2
+
+    [<Fact>]
+    let ``player 1 wins their top cards always win against player 2 top cards`` () =
+        winnerOfGame [ (Club, Value 3) ] [ (Club, Value 2) ]
+        |> should equal Player1
+
+    [<Fact>]
+    let ``player 2 wins their top cards always win against player 1 top cards`` () =
+        winnerOfGame [ (Club, Value 2) ] [ (Club, Value 3) ]
+        |> should equal Player2
+
+    [<Fact>]
+    let ``player 1 wins when at some point they have better cards then player 2`` () =
+        winnerOfGame [ (Club, Value 2); (Heart, Ace) ] [ (Club, Value 3); (Club, Value 4) ]
+        |> should equal Player1
+
+    [<Fact>]
+    let ``player 2 wins when at some point they have better cards then player 1`` () =
+        winnerOfGame [ (Club, Value 3); (Club, Value 4) ] [ (Club, Value 2); (Heart, Ace) ]
+        |> should equal Player2
+
+    [<Fact>]
+    let ``there is no game to be played when both players are handed no cards`` () =
+        shouldFail (fun () -> winnerOfGame [] [] |> ignore)
--- Wonderland/CardGameWar.fs
+++ Wonderland/CardGameWar.fs
@@ -23,7 +23,22 @@ module CardGameWar =
         else card2
 
 
-    let playGame (hand1: Card list, hand2: Card list) = failwith "not implemented: game winner"
+    type Winner =
+        | Player1
+        | Player2
+        | Draw
+
+    let private card1IsWinning card1 card2 = winningCard card1 card2 = card1
+
+    let private addToBottom hand card1 card2 = hand @ [ card1; card2 ]
+
+    let rec winnerOfGame (hand1: Card list) (hand2: Card list) =
+        match hand1, hand2 with
+        | [], [] -> failwith "there is no game to be played"
+        | _, [] -> Player1
+        | [], _ -> Player2
+        | c1 :: h1, c2 :: h2 when card1IsWinning c1 c2 -> winnerOfGame (addToBottom h1 c2 c1) h2
+        | c1 :: h1, c2 :: h2 (* otherwise card2 wins *) -> winnerOfGame h1 (addToBottom h2 c1 c2)
 
 (*
     let suits = [ Spade; Club; Diamond; Heart ]
